af3f1ecdb5081b652ff2f978361ce6fcd62eef10
juanmadelboca <juanmadelboca@gmail.com>
2017-09-18 03:19:24 -0300

    refactoring paint method

--- app/src/main/java/com/kalantos/spitball/activities/GameActivity.java
+++ app/src/main/java/com/kalantos/spitball/activities/GameActivity.java
@@ -200,7 +200,6 @@ public class GameActivity extends AppCompatActivity {
                     }
 
                     idR= getResources().getIdentifier(ballImage,"drawable",getPackageName());
-
                     //painting
                     ballSize =(int)((game.getTiles()[i][j].getBall().getSize()) + heightScreen/14 -
                             ((game.getTiles()[i][j].getBall().getSize())*(1/7)*((double)widthScreen/heightScreen)));
